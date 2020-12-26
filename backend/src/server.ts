d429c99b573761416b6b4d33b913b6a5396d7fad
Pedro Barão <phbarao@gmail.com>
2020-12-26 18:11:52 -0300

    small fixes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,7 @@
 import express from "express";
 
+import "./database/connection";
+
 const app = express();
 app.use(express.json());
 
@@ -8,13 +10,5 @@ Rota = conjunto / Recurso = usuário
 O primeiro parametro é a rota.
 O segundo é a função que diz o que fazer.
 */
-
-app.post("/users/:id", (request, response) => {
-  console.log(request.query);
-  console.log(request.params);
-  console.log(request.body);
-
-  return response.json({ message: "Hello!" });
-});
 
 app.listen(3333);
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import * as dotenv from 'dotenv';
 
 import './styles/global.css';
 import Routes from './routes';
+
+dotenv.config();
 
 function App() {
   return <Routes />;
