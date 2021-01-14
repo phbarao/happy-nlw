0048255af2d391f6c4ef408a412fd227d5614a71
Pedro Barão <phbarao@gmail.com>
2021-01-14 16:30:04 -0300

    fixing map on orphanage page

--- web/src/pages/Orphanage/index.tsx
+++ web/src/pages/Orphanage/index.tsx
@@ -85,7 +85,7 @@ export default function Orphanage() {
                 doubleClickZoom={false}
               >
                 <TileLayer
-                  url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                  url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicGVkcm9iYXJvbmVzIiwiYSI6ImNrajBuNHFjejBpem8yeXBod210eDlheHkifQ.2SMaao8ed7F9ttxXJ1533A`}
                 />
                 <Marker
                   interactive={false}
