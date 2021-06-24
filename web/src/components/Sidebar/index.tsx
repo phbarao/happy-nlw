88382971d3c7161d356e01c95d85f8b8fe6087b8
Pedro Barão <phbarao@gmail.com>
2021-06-23 22:06:02 -0300

    refactoring

--- web/src/components/Sidebar/index.tsx
+++ web/src/components/Sidebar/index.tsx
@@ -8,9 +8,11 @@ import mapMarkerImg from "../../images/map-marker.svg";
 
 export default function Sidebar() {
   const { goBack } = useHistory();
+
   return (
     <aside className="app-sidebar">
       <img src={mapMarkerImg} alt="Happy" />
+
       <footer>
         <button type="button" onClick={goBack}>
           <FiArrowLeft size={24} color="#FFF" />
